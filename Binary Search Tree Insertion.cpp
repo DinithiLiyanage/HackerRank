97d270e90d54dce7c3a3be9924bd94ce154b6f8a
DinithiLiyanage <dinithi.adithya@gmail.com>
2023-07-12 13:48:24 +0530

    Create Binary Search Tree Insertion.cpp

---
+++ Binary Search Tree Insertion.cpp
@@ -0,0 +1,30 @@
+/*
+Node is defined as 
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    Node * insert(Node * root, int data) {
+        if(root == NULL){
+            Node *new_node = new Node(data);
+            return new_node;
+        }
+        else if(data < root->data){
+            root->left = insert(root->left, data);
+        }
+        else{
+            root->right = insert(root->right, data);
+        }
+        return root;
+    }
